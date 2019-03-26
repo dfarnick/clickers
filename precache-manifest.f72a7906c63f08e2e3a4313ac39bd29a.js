f365c6f444e3fa80f7a424b3175109c1eba8f06c
DavidFarnick <dfarnick2@gmail.com>
2019-03-25 21:53:16 -0500

    Updates

---
+++ precache-manifest.f72a7906c63f08e2e3a4313ac39bd29a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ec36fe43e34e545c279",
+    "url": "/clickers/static/js/runtime~main.a7b1029f.js"
+  },
+  {
+    "revision": "9981649ee429d0c4e921",
+    "url": "/clickers/static/js/main.4943cfb9.chunk.js"
+  },
+  {
+    "revision": "85f1719d56c8f7d51e2c",
+    "url": "/clickers/static/js/2.278c4d8b.chunk.js"
+  },
+  {
+    "revision": "9981649ee429d0c4e921",
+    "url": "/clickers/static/css/main.1577af42.chunk.css"
+  },
+  {
+    "revision": "4b29520e1841d763fabdb52660b805cb",
+    "url": "/clickers/index.html"
+  }
+];
